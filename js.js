67ab23db9bbae9293fea397877b8bb11413c16d4
Saneesh NT <148613852+saneeshnt@users.noreply.github.com>
2026-01-16 14:22:26 +0530

    Delete js.js

--- js.js
+++
@@ -1,5 +0,0 @@
- function handleClick(){
-            document.querySelector("body>button").innerText="in progress "
-                        document.getElementById("heading").style.color="red"
-
-        }
